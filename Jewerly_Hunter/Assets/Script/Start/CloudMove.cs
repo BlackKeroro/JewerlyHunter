09ecb97e24b87826996357247bab691cb4e1be66
BlackKeroro <97724010+BlackKeroro@users.noreply.github.com>
2022-03-23 16:17:27 +0900

    3/23
    Cloud Move

--- Jewerly_Hunter/Assets/Script/Start/CloudMove.cs
+++ Jewerly_Hunter/Assets/Script/Start/CloudMove.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class CloudMove : MonoBehaviour
 {
+    public float Speed;
 
     // Start is called before the first frame update
     void Start()
     {
+        Speed = Random.Range(2f, 4f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Random.Range(0.01f, 0.005f), 0, 0);
+        transform.position += Vector3.right * Speed * Time.deltaTime;
         if(transform.position.x > 20.0f)
         {
             Destroy(gameObject);
